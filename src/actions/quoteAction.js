4b502e350732ca23d2b9c3c07bf542b068180e69
StrelArt <streltsyn.artem@gmail.com>
2024-12-09 19:59:13 +0200

    add logger and thunk enhancers

--- src/actions/quoteAction.js
+++ src/actions/quoteAction.js
@@ -1,6 +1,19 @@
 export const PUT_QUOTE = 'PUT_QUOTE';
 
-export const putQuote = quote => ({
+export const putQuote = quote => {
+    return {
     type: PUT_QUOTE,
     payload: quote
-})
+}}
+
+
+export const fetchQuote = () => {
+    return dispatch => {
+        dispatch(putQuote('Pending...'))
+        fetch('https://api.gameofthronesquotes.xyz/v1/random')
+            .then(response => response.json())
+            .then(data => dispatch(putQuote(data.sentence)))
+            .catch(() => dispatch(putQuote('Failed to fetch quote data.')));
+
+    }
+}
