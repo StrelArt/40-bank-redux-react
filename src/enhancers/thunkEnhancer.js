f4fae344c0c0fe816414cfff3d3fc543db09cc69
StrelArt <streltsyn.artem@gmail.com>
2024-12-09 20:15:29 +0200

    add redux-logger and redux-thunk enhancers

--- src/enhancers/thunkEnhancer.js
+++
@@ -1,6 +0,0 @@
-export const thunkEnhancers = ({getState, dispatch}) => next => action => {
-    if(typeof action === 'function') {
-        return action(dispatch, getState);
-    }
-    return next(action);
-}
